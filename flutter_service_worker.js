3e02201c68f987c344fb304df7e47fef412ea27c
NduProject <stackone@nduprojectoutlook.onmicrosoft.com>
2025-11-05 19:08:19 +0200

    This is push number two the first didn't show the logo

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,7 +38,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "eb9078acc215761bd0e6da323a395250
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "bd8755efda7eab1fabb0c83d9070b452",
+"flutter_bootstrap.js": "2e99099864b5263814bfad7ecd1912c0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
